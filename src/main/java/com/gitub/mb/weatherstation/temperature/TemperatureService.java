c130dd8f9b0a1b845b3485783d63e5d31348bdc7
meRybaczek <wojciech.rybaczek@gmail.com>
2023-02-04 10:23:38 +0100

    temperatutre Service upgraded,TemperatureControllerIT upgraded

--- src/main/java/com/gitub/mb/weatherstation/temperature/TemperatureService.java
+++ src/main/java/com/gitub/mb/weatherstation/temperature/TemperatureService.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TemperatureService {
+    TemperatureRepository temperatureRepository;
 
     public TemperatureService(TemperatureRepository temperatureRepository) {
+        this.temperatureRepository = temperatureRepository;
 
     }
 
-    public WeatherPoint retrieveTemperature() {
-        return new WeatherPoint(null,null);
+    public WeatherPoint retrieveTemperature()  {
+        return temperatureRepository.findTopByOrderByIdDesc()
+                .orElseThrow(RuntimeException::new);
     }
 }
