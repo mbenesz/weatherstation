d9eb0f1833b8a0bfa88a2cf0d7e64c20680c499f
meRybaczek <wojciech.rybaczek@gmail.com>
2023-03-24 17:17:11 +0100

    annotation @RestControllerAdvise added

--- src/main/java/com/gitub/mb/weatherstation/temperature/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/gitub/mb/weatherstation/temperature/RestResponseEntityExceptionHandler.java
@@ -4,9 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
-
+@RestControllerAdvice
 public class RestResponseEntityExceptionHandler {
 
     @ResponseBody
