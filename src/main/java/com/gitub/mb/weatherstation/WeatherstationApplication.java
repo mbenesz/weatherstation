f3d93b9149fa72d16e40598cc8ffc1de8212d8c4
meRybaczek <wojciech.rybaczek@gmail.com>
2023-03-29 21:16:47 +0200

    no bean found for cache bug fixed

--- src/main/java/com/gitub/mb/weatherstation/WeatherstationApplication.java
+++ src/main/java/com/gitub/mb/weatherstation/WeatherstationApplication.java
@@ -3,8 +3,10 @@ package com.gitub.mb.weatherstation;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -24,6 +26,11 @@ public class WeatherstationApplication {
 	@Bean
 	public ObjectMapper objectMapper() {
 		return new ObjectMapper();
+	}
+
+	@Bean
+	public CacheManager cacheManager() {
+		return new ConcurrentMapCacheManager("retrieveTemperature");
 	}
 
 }
